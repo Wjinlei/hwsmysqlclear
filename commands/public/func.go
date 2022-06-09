b121959b0cce3e3a9926275fa5c383e1e7715e21
Jerry Wang <freebufer.wang@gmail.com>
2022-06-09 16:55:53 +0800

    refactor: FindScript.

--- commands/public/func.go
+++ commands/public/func.go
@@ -3,40 +3,51 @@ package public
 import (
 	"fmt"
 	"regexp"
+
+	"github.com/Wjinlei/golib"
 )
 
+var regularizer *regexp.Regexp
+
+func init() {
+	regularizer = regexp.MustCompile(`(?i)<script.*(</script[^>]*>)?`)
+}
+
 func (conn *connect) FindScript(table string) error {
-	rows, cols, callback, err := conn.QueryRows("SELECT * FROM `" + table + "`")
+	rows, columns, callback, err := conn.QueryRows("SELECT * FROM `" + table + "`")
 	if err != nil {
 		return err
 	}
 
-	regularizer := regexp.MustCompile(`(?i)<script.*(</script[^>]*>)?`)
-
 	for rows.Next() {
-		for i := range cols {
-			columnValue := callback(i)
-			findAllCase := regularizer.FindAllString(columnValue, -1)
-			for _, findCase := range findAllCase {
-				conn.replace(table, cols[i], columnValue, findCase, "")
+		// Take the 0th field as the unique identifier
+		id := fmt.Sprintf("%s=%s", columns[0], callback(0))
+
+		for i := range columns {
+			value := callback(i)
+			for _, findCase := range regularizer.FindAllString(value, -1) {
+				conn.replace(table, id, columns[i], value, findCase, "")
 			}
 		}
 	}
 	return nil
 }
 
-func (conn *connect) replace(table string, column string, value string, oldCase string, newCase string) error {
+func (conn *connect) replace(table string, id string, column string, value string, oldCase string, newCase string) error {
 	query := fmt.Sprintf("UPDATE `%s` SET `%s` = REPLACE(`%s`, ?, ?) WHERE `%s` = ?", table, column, column, column)
-
 	stmt, err := conn.db.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
-
 	_, err = stmt.Exec(oldCase, newCase, value)
 	if err != nil {
 		return err
 	}
+
+	// Log
+	today := golib.FormatNowTime("2006-01-02")
+	content := fmt.Sprintf("[%s] 表: %s\t字段: %s\t标识: %s\t原内容: %s\n", today, table, column, id, value)
+	golib.FileWrite(today+".log", content, golib.FileAppend)
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Wjinlei/hwsmysqlclear
 go 1.18
 
 require (
+	github.com/Wjinlei/golib v0.0.0-20220512031025-69f9ab3861ab
 	github.com/genshen/cmds v0.0.0-20200505065256-d4c52690e15b
 	github.com/go-sql-driver/mysql v1.6.0
 )
